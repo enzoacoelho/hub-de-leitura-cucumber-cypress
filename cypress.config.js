8d7856b41d2f98faeb20c9ece9c94d919c812696
enzoacoelho <enzoaugustocoelho@outlook.com>
2026-03-03 17:59:39 -0300

    configurando allure report e adicionando arquivo jenkins file

--- cypress.config.js
+++ cypress.config.js
@@ -2,17 +2,29 @@ const { defineConfig } = require('cypress')
 const createBundler = require('@bahmutov/cypress-esbuild-preprocessor')
 const { addCucumberPreprocessorPlugin } = require('@badeball/cypress-cucumber-preprocessor')
 const { createEsbuildPlugin } = require('@badeball/cypress-cucumber-preprocessor/esbuild')
+const { allureCypress } = require('allure-cypress/reporter')
 
 module.exports = defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.feature',
+    baseUrl: 'http://localhost:3000/',
+    video: true,
+
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config)
-      on('file:preprocessor', createBundler({
-        plugins: [createEsbuildPlugin(config)]
-      }))
+
+      on(
+        'file:preprocessor',
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      )
+
+      allureCypress(on, config, {
+        resultsDir: 'allure-results',
+      })
+
       return config
     },
-    baseUrl: 'http://localhost:3000/'
   },
 })
